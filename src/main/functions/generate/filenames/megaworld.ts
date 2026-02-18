785e5f902930c8ea5178eaea8c06efea66d316f3
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2026-02-19 03:13:17 +0800

    update: megaworld report output into txt

--- src/main/functions/generate/filenames/megaworld.ts
+++ src/main/functions/generate/filenames/megaworld.ts
@@ -38,7 +38,7 @@ export const generateMWFilename = (
     const month = currentDate.getMonth() + 1
     const monthCode = month > 9 ? String.fromCharCode(64 + month - 9) : String(month)
     const day = String(currentDate.getDate()).padStart(2, '0')
-    return `${type}${formattedPartnerCode}${formattedTerminal}${batchNo}.${monthCode}${day}`
+    return `${type}${formattedPartnerCode}${formattedTerminal}${batchNo}.${monthCode}${day}.txt`
   } catch (error: any) {
     console.error(error.message)
     return 'error.txt'
